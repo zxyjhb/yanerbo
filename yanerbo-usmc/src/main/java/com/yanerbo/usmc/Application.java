c51a39222ca46113ca6513d295d7d4ec941e2bba
jihaibo <568025297@qq.com>
2018-11-13 22:23:52 +0800

    cmt

---
+++ yanerbo-usmc/src/main/java/com/yanerbo/usmc/Application.java
@@ -0,0 +1,21 @@
+package com.yanerbo.usmc;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * 这里需要添加@EnableEurekaServer  去启动Spring cloud eureka注册服务
+ * @author jihaibo
+ *
+ */
+@SpringBootApplication
+@EnableEurekaServer
+public class Application {
+	
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+}
